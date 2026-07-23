5f2079843a07606e6ff3263150ae68fa58fff1b7
Ayush Anand <ayushanand16333@gmail.com>
2026-07-24 02:10:33 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    int x=0;
+    for(int i=0;i<n;i++){
+        string temp;
+        cin>>temp;
+        if(temp=="++X")++x;
+        else if(temp=="X++")x++;
+        else if(temp=="--X")--x;
+        else x--;
+    }
+    cout<<x;
+}
