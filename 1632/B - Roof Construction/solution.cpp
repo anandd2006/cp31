a2a42c45be4540a8758b1aa3471adfb7f7233f10
Ayush Anand <ayushanand16333@gmail.com>
2026-08-12 14:53:29 +0530

    Add Roof Construction [B] from Codeforces

---
+++ 1632/B - Roof Construction/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+ 
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+void solve() {
+    int n;
+    cin>>n;
+    int p=1;
+    while(p*2<n)p*=2;
+    for(int i=n-1;i>=p;i--)cout<<i<<" ";
+    for(int i=0;i<p;i++)cout<<i<<" ";
+    cout<<endl;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
