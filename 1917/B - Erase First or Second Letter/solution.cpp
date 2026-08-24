cecc8cecc06fc7d17a123ee3813b9d8a63309f03
Ayush Anand <ayushanand16333@gmail.com>
2026-08-24 17:20:18 +0530

    Add Erase First or Second Letter [B] from Codeforces

---
+++ 1917/B - Erase First or Second Letter/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    set<char> d;
+    ll ans=0;
+    for(char c:s){
+        d.insert(c);
+        ans+=d.size();
+    }
+    cout<<ans<<"
+";
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
