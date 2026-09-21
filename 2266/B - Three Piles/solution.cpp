701ca98dbfe7c871af38a79f21661bc0e327358b
Ayush Anand <ayushanand16333@gmail.com>
2026-09-21 20:36:37 +0530

    Add Three Piles [B] from Codeforces

---
+++ 2266/B - Three Piles/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+void solve() {
+    ll a,b,c;
+    cin>>a>>b>>c;
+    cout<<max(abs((a+c)-b),abs(a-b))<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
